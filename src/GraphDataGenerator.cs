133bb0002a16893bbba31aacd0e18512c38b63a6
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2020-01-13 23:06:27 +0100

    Polynomials

--- src/GraphDataGenerator.cs
+++ src/GraphDataGenerator.cs
@@ -6,5 +6,33 @@ namespace CubicSplineInterpolation.src
 {
     class GraphDataGenerator
     {
+
+        List<Point> points;
+
+
+        public List<Point> Generate(List<Point> points, Vector polynomials, int intervalsBetweenPoints)
+        {
+            this.points = points;
+            List<Point> graph = new List<Point>();
+
+
+
+            return graph;
+        }
+
+        private int WhichPolynomialDescribes(double x)
+        {
+            for (int i = 1; i < points.Count; i++)
+            {
+                if(x < points[i].x)
+                {
+                    return i - 1;
+                }
+            }
+            throw new IndexOutOfRangeException();
+        }
+
+
+
     }
 }
---
+++ src/Polynomial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CubicSplineInterpolation
+{
+    public class Polynomial
+    {
+        double[] variables;
+
+        public double A
+        {
+            get => variables[0];
+        }
+
+        public double B
+        {
+            get => variables[1];
+        }
+
+        public double C
+        {
+            get => variables[2];
+        }
+
+        public double D
+        {
+            get => variables[3];
+        }
+
+
+        public Polynomial(double[] variables)
+        {
+            this.variables = variables;
+        }
+
+        public Polynomial(double a, double b, double c, double d)
+        {
+            this.variables = new double[4] { a, b, c, d };
+        }
+    }
+}
